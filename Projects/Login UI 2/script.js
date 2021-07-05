0f7c7a238e9c60b79bfd51dfa738032f6d386c57
abyusufi <abdurrahmanyusufi478@gmail.com>
2021-07-05 22:41:14 +0530

    Login UI 2 is now responsive

--- Projects/Login UI 2/script.js
+++ Projects/Login UI 2/script.js
@@ -1,11 +1,10 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 const signupBtn = document.querySelector("#signup");
 const signinBtn = document.querySelector("#signin");
-
-signupBtn.addEventListener('click', () => {
+signupBtn.addEventListener("click", () => {
   container.classList.toggle("signup-mode");
-})
+});
 
-signinBtn.addEventListener('click', () => {
+signinBtn.addEventListener("click", () => {
   container.classList.toggle("signup-mode");
-})
+});
